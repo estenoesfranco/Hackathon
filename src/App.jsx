63bcee591723329eb83d7dfbea8269215b5937fd
juliussn <cabrera1308julian@gmail.com>
2024-09-03 10:23:26 -0300

    Co-authored-by: estenoesfranco <estenoesfranco@users.noreply.github.com> Co-authored-by: galalroenzo860 <galalroenzo860@users.noreply.github.com> Co-authored-by: LoreGal24 <LoreGal24@users.noreply.github.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
+  useEffect(()=>{
+    fetch('https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&appid=fe4ed6407da8fdf33dce908d359ef45a')
+        .then(res=>res.json())
+            .then(json=>{setProductos(json)})
+            console.log(productos)
+},[]);
+
   return (
     <>
-    <div></div>
+    <div>
+      <h1>Hola Mundo!</h1>
+    </div>
     </>
   )
 }
